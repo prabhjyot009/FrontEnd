c972566407349bc1f35acbe6f708cc6402a259bf
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 11:34:04 +0545

    Math operators in js

---
+++ JavaScript/College Practice/MathOperator.js
@@ -0,0 +1,9 @@
+let num1 = 100;
+let num2 = 200;
+console.log("Add:", num1 + num2);
+console.log("Add:", num1 - num2);
+console.log("Add:", num1 * num2);
+console.log("Add:", num1 / num2);
+console.log("Add:", num1 ** 2);
+console.log("Add:", ++num1);
+console.log("Add:", --num1);
