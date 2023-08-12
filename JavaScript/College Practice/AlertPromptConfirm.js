e85c3c67cfe623f7ed2897083146f2682c160f5d
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 10:42:14 +0545

    Alert, Prompt and confirm functionalities

---
+++ JavaScript/College Practice/AlertPromptConfirm.js
@@ -0,0 +1,8 @@
+// alert("Hello")
+// alert({'a':'b'});
+
+// let result=confirm('Are you sure you wanna delete this?');
+// console.log(result);
+
+let result=prompt('Are you sure you wanna delete this');
+console.log(result);
