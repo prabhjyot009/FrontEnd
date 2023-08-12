1c4cb9d8b0162bb20c4c6102146363706584a3bd
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 12:16:29 +0545

    While and Do-While loop in js

---
+++ JavaScript/College Practice/WhileLoop.js
@@ -0,0 +1,14 @@
+var i =0;
+// while (i<100) {
+//     console.log(i);
+//     i++
+// }
+// console.log(i);
+
+//Do-while loop
+do{
+    console.log('great');
+    i++;
+}while(i<10);
+
+console.log(i);
