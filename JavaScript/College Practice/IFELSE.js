681aa38eb00ed2236cda6ef6113ebd3c4ade19d5
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 11:21:42 +0545

    If else in js

---
+++ JavaScript/College Practice/IFELSE.js
@@ -0,0 +1,12 @@
+let myName="singh";
+myName="brar";
+
+if(myName=="singh"){
+    console.log("Same");
+}
+else if(myName=="brar"){
+    console.log("Value of variable changed");
+}
+else{
+    console.log("????");
+}
