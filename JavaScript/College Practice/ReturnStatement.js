dcb935046ae037f666d0e004d51731f3ce274fa4
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 09:54:40 +0545

    Return Statement

---
+++ JavaScript/College Practice/ReturnStatement.js
@@ -0,0 +1,15 @@
+function myAwesomeFucntion(myString,myString2=0){
+// return 100;
+// return true;
+// return {key:"value"};
+
+// return function(){
+//     console.log("hey i'm a fucntion")
+// };
+
+    console.log(myString);
+    console.log(myString2);
+}
+let myResult=myAwesomeFucntion("abc","def");
+
+console.log(myResult());
