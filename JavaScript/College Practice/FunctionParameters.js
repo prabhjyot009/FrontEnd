bd243661759dcb7d7e40c6ff5ae94048c3709755
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 09:49:23 +0545

    Function parameters

---
+++ JavaScript/College Practice/FunctionParameters.js
@@ -0,0 +1,6 @@
+function myAwesomeFucntion(myString,myString2=0){
+    console.log(myString);
+    console.log(myString2);
+}
+myAwesomeFucntion("Hello","one");
+myAwesomeFucntion(12345,"my custom value");
