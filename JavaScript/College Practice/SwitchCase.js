0791456fb18535b15215e5f980e772db741d667e
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 11:25:27 +0545

    Switch Case in js

---
+++ JavaScript/College Practice/SwitchCase.js
@@ -0,0 +1,11 @@
+let myAge=21;
+switch (myAge) {
+    case 20:
+        console.log('You are 20');
+        break;
+    case 21:
+        console.log('You are 21');
+    default:
+        console.log('You are neither 20 nor 21');
+        break;
+}
