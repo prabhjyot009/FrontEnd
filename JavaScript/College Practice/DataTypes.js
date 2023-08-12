15fe5ac94d36981b34e4b06698e40640953f8ae6
Prabhjyot Singh <prabh156dps@gmail.com>
2023-08-12 09:17:26 +0545

    Datatypes in js

---
+++ JavaScript/College Practice/DataTypes.js
@@ -0,0 +1,21 @@
+let string="This is normal string";
+let number=26;
+let bool=false;
+let array=[1,2,3,4,5,"a","b","c"];//collection of dataypes
+
+let _undefined=undefined;
+let _null=null;
+let _NaN=NaN;
+
+let object={
+    "keyname":"value"
+};
+
+//string
+//number
+//bool
+//array[]
+//array[4]=5
+//array[5]="a"
+// object.keyname same as object["keyname"]
+// object["key name"]
