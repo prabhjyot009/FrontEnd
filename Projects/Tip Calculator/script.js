b2360288a9f23d97f5402e5d845de2dae5a9d4b0
prabhjyot009 <prabh156dps@gmail.com>
2023-08-14 22:53:11 +0530

    Tip Calculator completed

--- Projects/Tip Calculator/script.js
+++ Projects/Tip Calculator/script.js
@@ -8,6 +8,6 @@ btn.addEventListener("click", function () {
     const cost=document.querySelector("input");
     console.log(cost.value*0.15);
     let tip=(cost.value*0.15).toFixed(2);
-    let temp='<h1>Your should tip $'+tip+'on $'+cost.value+'</h1>';
+    let temp=`<h1>You should tip $${tip} on $${cost.value}</h1>`;
     output.innerHTML=temp;
 })
