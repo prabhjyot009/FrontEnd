82751f93ef3c77afb2692ba539aa3d8a90da2e7b
prabhjyot009 <prabh156dps@gmail.com>
2023-08-14 22:34:18 +0530

    commit

--- Projects/Tip Calculator/script.js
+++ Projects/Tip Calculator/script.js
@@ -1,3 +1,14 @@
 //in innerHTML we are allow to do html scripting
 //where as innerTEXT only outputs it as text format
 
+const btn=document.querySelector("button");
+console.log(button);
+// console.log(button[0]);
+
+// button.innerHTML="HELLO";
+// button[0].innerHTML="HELLO";
+
+//adding an event listener
+btn.addEventListener("click",function () {
+    console.log("click")
+})
